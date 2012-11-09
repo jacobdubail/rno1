3ff56a1469d2c0bd3650b1aee96c3cc924222f93
Jacob Dubail <jacobdubail@gmail.com>
2012-11-09 14:15:11 -0800

    commiting from subl

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 
   <title><?php wp_title(''); ?></title>
   <meta name="google-site-verification" content="">
-  <meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/i/favicon.ico">
   <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/i/apple-touch-icon.png"> 
