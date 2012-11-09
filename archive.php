453777ffd1ee055fd9de43719adb23fa67d65a73
Jacob Dubail <jacobdubail@gmail.com>
2012-11-09 11:29:53 -0800

    commiting from subl

--- archive.php
+++ archive.php
@@ -6,11 +6,11 @@
 
       <h2><?php the_title(); ?></h2>
 
-      <?php get_template_part( '_/inc/meta' ); ?>
+      <?php get_template_part( 'inc/meta' ); ?>
 
       <div class="entry">
 
-        <?php the_content(); ?>
+        <?php the_excerpt(); ?>
 
       </div>
 
