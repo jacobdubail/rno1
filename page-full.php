96098087d1d39b1862c632e5087f6756543efe85
Jacob Dubail <jacobdubail@gmail.com>
2012-11-12 08:52:47 -0800

    commiting from subl

---
+++ page-full.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Full Width
+*/
+?>
+
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+		<article class="post" id="post-<?php the_ID(); ?>">
+
+			<header class="figcaption">
+				<h1 class="page_title"><?php the_title(); ?></h1>
+			</header>
+
+			<div class="entry">
+
+				<?php the_content(); ?>
+
+			</div>
+
+		</article>
+		
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
