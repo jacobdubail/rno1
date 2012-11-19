254855410f65e8718f9bc7a9045167330ed6af0b
Jacob Dubail <jacobdubail@gmail.com>
2012-11-19 14:55:07 -0800

    commiting from subl

--- inc/meta.php
+++ inc/meta.php
@@ -1,5 +1,12 @@
-<p class="meta">
+<div class="meta">
 	<time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('n.j.y') ?></time>
 	&mdash;
   <?php swift_list_cats(4); ?>
-</p>
+  <?php if ( is_single() ) : ?>
+    <!-- <div class="sharing">
+      <div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" data-font="arial"></div>
+      <div class="g-plusone" data-size="small"></div>
+      <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="livethebrand">Tweet</a>
+    </div> -->
+  <?php endif; ?>
+</div>
