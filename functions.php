ea1d7e1d7e77f0df52d549e977b30855935c6ddb
Jacob Dubail <jacobdubail@gmail.com>
2012-11-19 16:47:00 -0800

    commiting from subl

--- functions.php
+++ functions.php
@@ -412,13 +412,14 @@ function swift_list_cats( $num = 4 ) {
 
 
 function rno1_get_excerpt($pid){
-  $excerpt = get_the_content($pid);
-  $excerpt = preg_replace(" (\[.*?\])",'',$excerpt);
-  $excerpt = strip_shortcodes($excerpt);
-  $excerpt = strip_tags($excerpt);
-  $excerpt = substr($excerpt, 0, 220);
-  $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
-  $excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
+  $excerpt  = get_the_content($pid);
+  $excerpt  = preg_replace(" (\[.*?\])",'',$excerpt);
+  $excerpt  = strip_shortcodes($excerpt);
+  $excerpt  = strip_tags($excerpt);
+  $excerpt  = substr($excerpt, 0, 220);
+  $excerpt  = substr($excerpt, 0, strripos($excerpt, " "));
+  $excerpt  = trim(preg_replace( '/\s+/', ' ', $excerpt));
+  $excerpt .= "..."; 
 //  $excerpt = $excerpt.'... <a href="'.$permalink.'">more</a>';
   return $excerpt;
 }
