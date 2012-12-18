6f8bdcaf02f4eb195d5717b926e46d29de32d082
Jacob Dubail <jacobdubail@gmail.com>
2012-12-18 10:00:37 -0800

    commiting from subl

--- footer.php
+++ footer.php
@@ -4,14 +4,14 @@
     <?php dynamic_sidebar('Footer Widgets'); ?>
   </footer>
 
-  
+
 
 </div>
 <div id="fb-root"></div>
 <?php wp_footer(); ?>
 
 <script>
-  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+  var _gaq=[['_setAccount','UA-22136121-1'],['_trackPageview']];
   (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
   g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
   s.parentNode.insertBefore(g,s)}(document,'script'));
