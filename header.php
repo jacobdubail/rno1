21dd11ca404a153b5917ba1f3c4ecc4b506e5dc8
Jacob Dubail <jacobdubail@gmail.com>
2012-12-18 09:56:31 -0800

    commiting from subl

--- header.php
+++ header.php
@@ -6,15 +6,15 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  
+
   <?php if (is_search()) { ?>
-    <meta name="robots" content="noindex, nofollow" /> 
+    <meta name="robots" content="noindex, nofollow" />
   <?php } ?>
 
-  <title><?php wp_title(''); ?></title>
+  <title><?php wp_title('|'); ?></title>
   <meta name="google-site-verification" content="">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  
+
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
   <!-- <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon.png">  -->
 
@@ -30,16 +30,16 @@
   <!--[if (lt IE 9) & (!IEMobile)]>
 <script src="js/selectivizr-min.js"></script>
 <![endif]-->
-  
+
 </head>
 
 <body <?php body_class(); ?>>
   <!--[if lt IE 7]>
     <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
   <![endif]-->
-  
+
 <div class="page-wrap">
-  
+
   <header class="header cf">
     <h1><a id="logo" href="<?php home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
   </header>
@@ -58,6 +58,5 @@
       </ul>
     <?php endif; ?>
   </nav>
-  
+
   <section class="main-content cf grid">
-    
