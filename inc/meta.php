bbdfa449e96eb8d75f5acae0ec5203deea34cc0e
Jacob Dubail <jacobdubail@gmail.com>
2012-12-19 11:51:17 -0800

    commiting from subl

--- inc/meta.php
+++ inc/meta.php
@@ -3,10 +3,10 @@
 	&mdash;
   <?php swift_list_cats(4); ?>
   <?php if ( is_single() ) : ?>
-    <!-- <div class="sharing">
+    <div class="sharing">
       <div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" data-font="arial"></div>
       <div class="g-plusone" data-size="small"></div>
       <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="livethebrand">Tweet</a>
-    </div> -->
+    </div>
   <?php endif; ?>
 </div>
