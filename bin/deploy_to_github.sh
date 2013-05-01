b1c9b3b9f492a75f1a6c808148b9d86f6d420d9f
Jacob Dubail <jacob@jacobdubail.com>
2013-05-01 14:39:04 -0700

    commiting from subl

--- bin/deploy_to_github.sh
+++ bin/deploy_to_github.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git add . && git commit -am "commiting from subl" && git push origin && ssh root@rno1.com "cd /var/www/vhosts/rno1.com/httpdocs/wp-content/themes/rno1 && git pull"
+git add . && git commit -am "commiting from subl" && git push origin master && ssh root@rno1.com "cd /var/www/vhosts/rno1.com/httpdocs/wp-content/themes/rno1 && git pull"
