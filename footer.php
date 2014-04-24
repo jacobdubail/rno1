ac8c79222e3c5f25d9f1a9a7d1ddf33fb97838c4
Jacob Dubail <jacobdubail@gmail.com>
2014-04-24 11:02:23 -0400

    update GA

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 <?php wp_footer(); ?>
 
 <script>
-  var _gaq=[['_setAccount','UA-22136121-1'],['_trackPageview']];
+  var _gaq=[['_setAccount','UA-50306549-1'],['_trackPageview']];
   (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
   g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
   s.parentNode.insertBefore(g,s)}(document,'script'));
